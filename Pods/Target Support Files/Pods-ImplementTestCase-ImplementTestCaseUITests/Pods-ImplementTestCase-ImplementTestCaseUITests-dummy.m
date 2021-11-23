730b800b079a0d1be07b6d93cf3bbfb156aa4fa0
Prit <prit.sensussoft@gmail.com>
2021-11-23 11:13:48 +0530

    workspace

---
+++ Pods/Target Support Files/Pods-ImplementTestCase-ImplementTestCaseUITests/Pods-ImplementTestCase-ImplementTestCaseUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ImplementTestCase_ImplementTestCaseUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ImplementTestCase_ImplementTestCaseUITests
+@end
---
+++ Pods/Target Support Files/Pods-ImplementTestCaseTests/Pods-ImplementTestCaseTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ImplementTestCaseTests : NSObject
+@end
+@implementation PodsDummy_Pods_ImplementTestCaseTests
+@end
