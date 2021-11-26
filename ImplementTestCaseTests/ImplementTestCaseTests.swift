c0a5ec6fe533fb5a0f10051c012cc91a828c6d49
Prit <prit.sensussoft@gmail.com>
2021-11-26 09:56:37 +0530

    final checking

--- ImplementTestCaseTests/ImplementTestCaseTests.swift
+++ ImplementTestCaseTests/ImplementTestCaseTests.swift
@@ -8,7 +8,7 @@ class ImplementTestCaseTests: XCTestCase {
     //MARK:- TestCase Implement
 
     func testemailEmpty(){
-        let  result = vc.validLoginInput(email: "", password: "Aa@12345")
+        let  result = vc.validLoginInput(email: "abc@gmail.com.uk", password: "Aa@12345")
         XCTAssertFalse(result)
     }
     
